20e5f0d995fc95dd41176f5af77ae0cb451510c9
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-28 01:36:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-      sorted_movies = Movie.where("release_date > 2002").order(release_date: :desc)
+      highest_rating = Show.order(rating: :desc)
   end
 
 
