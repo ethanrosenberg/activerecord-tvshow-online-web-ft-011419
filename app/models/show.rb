16f4b964b7e157984ca47a91496a15e7650dc6ae
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-28 01:37:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,5 +4,9 @@ class Show < ActiveRecord::Base
       highest_rating = Show.order(rating: :desc).first.rating
   end
 
+  def self.most_popular_show
+      highest_rating = Show.order(rating: :desc).first.rating
+  end
+
 
 end
